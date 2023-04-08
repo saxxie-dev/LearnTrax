6e5926ec509df88ce18b7c25d5323f0155c12743
saxxie-dev <c@saxxie.dev>
2023-04-08 09:53:25 -0700

    update flake.nix to handle some node

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nix-npm-buildpackage = { url = "github:serokell/nix-npm-buildpackage"; };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nix-npm-buildpackage } @attrs:
+outputs = { self, nixpkgs, nixpkgs-unstable, nix-npm-buildpackage } @attrs:
     let
       system = "x86_64-linux";
       overlay-unstable = final: prev: {
@@ -17,12 +17,41 @@
         #  config.allowUnfree = true;
         # };
       };
+      nixPackages = nixpkgs.legacyPackages.${system};
+      buildPackage = nixPackages.callPackage nix-npm-buildpackage { };
+      nodeDependencies = buildPackage.mkNodeModules {
+        # path to where the package-lock.json file is located
+        src = ./.;
+        pname = "npm";
+        version = "8";
+      };
+      frontendAssets = nixPackages.stdenv.mkDerivation
+        (
+          {
+            name = "frontend-assets";
+            buildInputs = [ nixPackages.esbuild nixPackages.nodePackages_latest.tailwindcss ];
+            src = ./tailwind/.;
+            # Build scripts for your bundled frontend assets
+            installPhase = ''
+              export NODE_ENV=production
+              export NODE_PATH=${nodeDependencies}/node_modules
+              export NODE_ENV=production
+              export npm_config_cache=${nodeDependencies}/config-cache
+              export BUNDLE_PATH=$out/frontend-assets 
+              mkdir -p $BUNDLE_PATH
+              esbuild $src/app.jsx --bundle --outfile=$BUNDLE_PATH/app.js --minify 
+              # Navigating into the root project directory here so Tailwind gets to analyze the files with tailwind rules
+              cd ${./.} && tailwindcss -c tailwind/tailwind.config.js -o $BUNDLE_PATH/app.css --minify
+            '';
+          }
+        );
     in
     {
-      nixosConfigurations."LearnTracks" = nixpkgs.lib.nixosSystem {
+      nixosConfigurations."ihp-npm-shipnix" = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = attrs // {
           environment = "production";
+          frontendAssets = frontendAssets;
         };
         modules = [
           # Overlays-module makes "pkgs.unstable" available in configuration.nix
@@ -32,4 +61,3 @@
       };
     };
 }
-    
